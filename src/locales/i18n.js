0b44a343d80ae7b1f32a7b04862cd41cc8a9f4e5
Emilien Lambert <l.emilien@icloud.com>
2022-03-15 18:42:47 +0100

    add translations

---
+++ src/locales/i18n.js
@@ -0,0 +1,13 @@
+import { createI18n } from "vue-i18n";
+import en from "./en.json";
+import fr from "./fr.json";
+
+const i18n = createI18n({
+  locale: "fr",
+  messages: {
+    en: en,
+    fr: fr,
+  },
+});
+
+export default i18n;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
+import i18n from "./locales/i18n";
 
-createApp(App).mount("#app");
+createApp(App).use(i18n).mount("#app");
